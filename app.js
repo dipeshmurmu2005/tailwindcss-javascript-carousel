6a7db8c1e04bd8f8203530d9662d3c3a0a90bfb0
dipeshmurmu2005 <dipeshmurmu7@gmail.com>
2024-07-24 12:40:19 +0545

    adds options in constructor for slider

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ class Slider {
     /**
      * 
      * @param {string} sliderIdentifier - class of the slider elment 
+     * @param {Object} options - slider styles and features options 
      */
-    constructor(sliderIdentifier) {
+    constructor(sliderIdentifier, options) {
         this.class = sliderIdentifier;
+        this.options = options;
         this.build();
     }
     build() {
-        console.log(this.class);
+        console.log(this.options.pagination);
     }
 }
-let slider = new Slider('slider');
+let slider = new Slider('slider', {
+    pagination: false,
+});
